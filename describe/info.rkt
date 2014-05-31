53d734f9765bf45385cf1d055e078934b2f79168
Matthew Butterick <mb@mbtype.com>
2014-05-30 17:48:00 -0700

    repackage for earlier rackets

---
+++ describe/info.rkt
@@ -0,0 +1,14 @@
+#lang setup/infotab
+
+(define scribblings '(("scribblings/describe.scrbl" ())))
+(define blurb
+  (list "This library provides routines to describe Racket objects."))
+(define categories '(io misc))
+(define primary-file "main.rkt")
+(define release-notes
+  (list "The function float->string is now exported. It was also extended "
+        "to support big floats from the Math Library. Note that descriptions "
+        "of big floats do not include the exact decimal value because of the "
+        "possibility of extremely large value exhausting memory."))
+(define repositories '("4.x"))
+(define required-core-version "5.0")
